04ee88c17bd0ed98a20c3a0d0c0ce66936313cb8
Irham Maulana <i.maulana.azn@gmail.com>
2022-09-11 21:00:57 +0700

    finishing home page

--- components/organism/Footer/index.tsx
+++ components/organism/Footer/index.tsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import makeStyles from './style';
 
 export default function Footer() {
+  const classes = makeStyles();
   return (
-    <div>Footer</div>
+    <Box className={classes.footerContainer}>
+      <Box>
+        <img src="images/logo-header.svg" alt="" />
+      </Box>
+      <Box className={classes.footerContent}>
+        <Box className="listContainer">
+          <ul>
+            <li className="list__title"><a href="/#">Product</a></li>
+            <li><a href="/#">Build React components</a></li>
+            <li><a href="/#">Build Websites</a></li>
+            <li><a href="/#">Plugin for Figma</a></li>
+            <li><a href="/#">Docs</a></li>
+          </ul>
+        </Box>
+        <Box className="listContainer">
+          <Box>
+            <ul>
+              <li className="list__title"><a href="/#">Social</a></li>
+              <li><a href="/#">Facebook</a></li>
+              <li><a href="/#">Twitter</a></li>
+              <li><a href="/#">Medium</a></li>
+              <li><a href="/#">Instagram</a></li>
+            </ul>
+          </Box>
+          <Box>
+            <ul>
+              <li className="list__title"><a href="/#">Company</a></li>
+              <li><a href="/#">Jobs</a></li>
+              <li><a href="/#">Contact Us</a></li>
+            </ul>
+          </Box>
+        </Box>
+        <Box className="listContainer">
+          <Box>
+            <ul>
+              <li className="list__title"><a href="/#">Community</a></li>
+              <li><a href="/#">Discord</a></li>
+            </ul>
+          </Box>
+          <Box>
+            <ul>
+              <li className="list__title"><a href="/#">Terms</a></li>
+              <li><a href="/#">Privacy Policy</a></li>
+              <li><a href="/#">Terms of Service</a></li>
+            </ul>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- components/organism/Footer/style.js
+++ components/organism/Footer/style.js
@@ -2,9 +2,87 @@ import { makeStyles } from '@mui/styles';
 
 export default makeStyles(() => ({
   footerContainer: {
-
+    backgroundColor: '#171717',
+    color: 'white',
+    padding: '4rem 6rem 10rem',
   },
   footerContent: {
-
+    marginTop: '1rem',
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '1rem',
+    width: '100%',
+    justifyContent: 'space-between',
+    '& ul': {
+      listStyle: 'none',
+      padding: '0',
+      '& li a': {
+        textDecoration: 'none',
+        color: '#979797',
+        fontSize: '1.2rem',
+        lineHeight: '1.8',
+      },
+      '& li.list__title a': {
+        color: 'white',
+      },
+    },
+    '& .listContainer:first-child': {
+      width: '100%',
+      flex: 'none',
+      justifyContent: 'flex-start',
+    },
+    '& .listContainer': {
+      display: 'flex',
+      justifyContent: 'center',
+      gap: '1rem',
+      flex: '1',
+      '& div': {
+        flex: '1',
+      },
+    },
   },
+
+  '@media (min-width: 600px)': {
+    footerContainer: {
+      padding: '4rem 3rem 10rem',
+      display: 'flex',
+      '& :first-child': {
+        flex: '1',
+      },
+    },
+    footerContent: {
+      flex: '4',
+      margin: '0',
+      '& ul': {
+        margin: '0',
+        '& li a': {
+          fontSize: '1rem',
+        },
+      },
+      '& .listContainer:first-child': {
+        width: 'initial',
+      },
+      '& .listContainer': {
+        flexDirection: 'column',
+        justifyContent: 'start',
+        '& div': {
+          flex: 'initial',
+        },
+      },
+    },
+  },
+
+  '@media (min-width: 900px)': {
+    footerContainer: {
+      padding: '4rem 6rem 10rem',
+    },
+    footerContent: {
+      flex: 'initial',
+      justifyContent: 'space-evenly',
+      '& .listContainer': {
+        flex: 'initial',
+      },
+    },
+  },
+
 }));
