d7ea7febfaec52cc4d735cb4f518ea5f147d03d2
Irham Maulana <i.maulana.azn@gmail.com>
2022-10-31 14:39:48 +0700

    adding credits to the footer

--- components/organism/Footer/index.tsx
+++ components/organism/Footer/index.tsx
@@ -6,53 +6,67 @@ import makeStyles from './style';
 export default function Footer() {
   const classes = makeStyles();
   return (
-    <Box className={`${classes.footerContainer} container`}>
-      <Box>
-        <img src="images/logo-header.svg" alt="" />
+    <>
+      <Box className={`${classes.footerContainer} container`}>
+        <Box>
+          <img src="images/logo-header.svg" alt="" />
+        </Box>
+        <Box className={classes.footerContent}>
+          <Box className="listContainer">
+            <ul>
+              <li className="list__title"><a href="/#">Product</a></li>
+              <li><a href="/#">Build React components</a></li>
+              <li><a href="/#">Build Websites</a></li>
+              <li><a href="/#">Plugin for Figma</a></li>
+              <li><a href="/#">Docs</a></li>
+            </ul>
+          </Box>
+          <Box className="listContainer">
+            <Box>
+              <ul>
+                <li className="list__title"><a href="/#">Social</a></li>
+                <li><a href="/#">Twitter</a></li>
+                <li><a href="/#">Medium</a></li>
+                <li><a href="/#">Instagram</a></li>
+              </ul>
+            </Box>
+            <Box>
+              <ul>
+                <li className="list__title"><a href="/#">Company</a></li>
+                <li><a href="/#">Jobs</a></li>
+                <li><Link href="/contact"><a href="/#">Contact Us</a></Link></li>
+              </ul>
+            </Box>
+          </Box>
+          <Box className="listContainer">
+            <Box>
+              <ul>
+                <li className="list__title"><a href="/#">Community</a></li>
+                <li><a href="/#">Discord</a></li>
+              </ul>
+            </Box>
+            <Box>
+              <ul>
+                <li className="list__title"><a href="/#">Terms</a></li>
+                <li><a href="/#">Privacy Policy</a></li>
+                <li><a href="/#">Terms of Service</a></li>
+              </ul>
+            </Box>
+          </Box>
+        </Box>
       </Box>
-      <Box className={classes.footerContent}>
-        <Box className="listContainer">
-          <ul>
-            <li className="list__title"><a href="/#">Product</a></li>
-            <li><a href="/#">Build React components</a></li>
-            <li><a href="/#">Build Websites</a></li>
-            <li><a href="/#">Plugin for Figma</a></li>
-            <li><a href="/#">Docs</a></li>
-          </ul>
-        </Box>
-        <Box className="listContainer">
-          <Box>
-            <ul>
-              <li className="list__title"><a href="/#">Social</a></li>
-              <li><a href="/#">Twitter</a></li>
-              <li><a href="/#">Medium</a></li>
-              <li><a href="/#">Instagram</a></li>
-            </ul>
-          </Box>
-          <Box>
-            <ul>
-              <li className="list__title"><a href="/#">Company</a></li>
-              <li><a href="/#">Jobs</a></li>
-              <li><Link href="/contact"><a href="/#">Contact Us</a></Link></li>
-            </ul>
-          </Box>
-        </Box>
-        <Box className="listContainer">
-          <Box>
-            <ul>
-              <li className="list__title"><a href="/#">Community</a></li>
-              <li><a href="/#">Discord</a></li>
-            </ul>
-          </Box>
-          <Box>
-            <ul>
-              <li className="list__title"><a href="/#">Terms</a></li>
-              <li><a href="/#">Privacy Policy</a></li>
-              <li><a href="/#">Terms of Service</a></li>
-            </ul>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+      <div
+        className="credits"
+        style={{
+          width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center',
+        }}
+      >
+        <p style={{ borderBottom: '1px dashed black', backgroundColor: 'white' }}>
+          Built by
+          {' '}
+          <a target="_blank" href="https://irham-maulana.netlify.app" style={{ color: 'blue' }} rel="noreferrer">Irham Maulana </a>
+        </p>
+      </div>
+    </>
   );
 }
